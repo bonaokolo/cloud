3251883dcc3ba6d00ce887a2273da315cba1b9db
bonaokolo <bonaokolo@gmail.com>
2025-04-20 13:19:28 +0000

    Add cleaned main.tf without secrets

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-2" # Change this if needed
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MyVPC"
+  }
+}
